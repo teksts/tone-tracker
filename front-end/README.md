71b5dc38cf3ffc9fabf1ae9d5e6c440b18bff04d
teksts <mark.egelhoff1@gmail.com>
2023-09-26 13:08:37 -0700

    Initial commit

---
+++ front-end/README.md
@@ -0,0 +1,3 @@
+# Tone Tracker
+
+A simple application for visualizing the fundamental frequency of the human voice, to aid in Chinese language learning.
---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Universal love to all beings throughout time.</h1>
+    </>
+  )
+}
+
+export default App
